adcb88a04243bf9c0c58ec5a54921873d0633e67
Patrik Holmstrom <patrikh86@gmail.com>
2021-09-24 02:31:50 +0200

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +12,161 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const VideoPlayerScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController? _firstVideoController;
+  late VideoPlayerController? _secondVideoController;
+  late Future<void> _initFirstVideoPlayer;
+  late Future<void> _initSecondVideoPlayer;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _firstVideoController =
+        VideoPlayerController.asset('assets/video/video_demo.mp4');
+
+    _secondVideoController =
+        VideoPlayerController.asset('assets/video/tolk.mp4');
+
+    _firstVideoController!.addListener(() {
+      setState(() {});
+    });
+
+    //_firstVideoController!.setLooping(true);
+    _firstVideoController!.initialize().then((_) {
+      setState(() {});
+    });
+    _secondVideoController!.initialize().then((_) {
+      setState(() {});
+    });
+
+    _initFirstVideoPlayer = _firstVideoController!.initialize();
+    _initSecondVideoPlayer = _secondVideoController!.initialize();
+
+    _firstVideoController!.addListener(() {
+      if (_firstVideoController!.value.isPlaying) {
+        _secondVideoController!.play();
+      } else if (!_firstVideoController!.value.isPlaying) {
+        _secondVideoController!.pause();
+      }
     });
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _firstVideoController!.dispose();
+    _secondVideoController!.dispose();
+  }
+
+  bool isHiddenSecondvideo = true;
+
+  final double _playerButtonSize = 30;
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text(' \"2 in 1 video proof of concept\"'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: FutureBuilder(
+        future: _initFirstVideoPlayer,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Center(
+              child: Column(
+                  //crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 720,
+                      child: Column(children: [
+                        Stack(children: [
+                          AspectRatio(
+                            aspectRatio:
+                                _firstVideoController!.value.aspectRatio,
+                            child: VideoPlayer(_firstVideoController!),
+                          ),
+                          Visibility(
+                            visible: isHiddenSecondvideo,
+                            child: Positioned(
+                                right: 10,
+                                bottom: 5,
+                                child: FittedBox(
+                                  fit: BoxFit.fill,
+                                  clipBehavior: Clip.hardEdge,
+                                  child: SizedBox(
+                                      width: 200,
+                                      height: 150,
+                                      child:
+                                          VideoPlayer(_secondVideoController!)),
+                                )),
+                          )
+                        ]),
+                        Container(
+                          color: Colors.black87,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                  icon: Icon(
+                                    _firstVideoController!.value.isPlaying
+                                        ? Icons.pause
+                                        : Icons.play_arrow,
+                                    size: _playerButtonSize,
+                                    color: Colors.white,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _firstVideoController!.value.isPlaying
+                                          ? _firstVideoController!.pause()
+                                          : _firstVideoController!.play();
+                                    });
+                                  }),
+                              IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isHiddenSecondvideo = !isHiddenSecondvideo;
+                                  });
+                                },
+                                icon: Icon(
+                                  Icons.wash,
+                                  size: _playerButtonSize,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const Text(
+                            ' \"Proof of concept\" för att ha uppspelning av en video med teckentolking i en ruta i. Det är planerad att teckenrutan ska gå att flytta runt och förstora och förminska.\n Klicka på \"handen\" för att visa/dölja rutan ')
+                      ]),
+                    ),
+                  ]),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
