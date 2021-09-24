4ad835f2a99710c450f38e776b8df16fd7a58a2e
Patrik Holmstrom <patrikh86@gmail.com>
2021-09-24 15:54:07 +0200

    Better controls, sign language button and loop

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
+import './widget/sign_language_icon.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -61,9 +63,22 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
     _firstVideoController!.addListener(() {
       if (_firstVideoController!.value.isPlaying) {
-        _secondVideoController!.play();
+        setState(() {
+          _secondVideoController!.play();
+        });
       } else if (!_firstVideoController!.value.isPlaying) {
-        _secondVideoController!.pause();
+        setState(() {
+          _secondVideoController!.pause();
+        });
+      }
+      if (_firstVideoController!.value.isLooping) {
+        setState(() {
+          _secondVideoController!.setLooping(true);
+        });
+      } else if (!_firstVideoController!.value.isLooping) {
+        setState(() {
+          _secondVideoController!.setLooping(false);
+        });
       }
     });
   }
@@ -77,96 +92,119 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   bool isHiddenSecondvideo = true;
 
-  final double _playerButtonSize = 30;
+  final double _playerButtonSize = 25;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(' \"2 in 1 video proof of concept\"'),
-      ),
-      body: FutureBuilder(
-        future: _initFirstVideoPlayer,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Center(
-              child: Column(
-                  //crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 720,
-                      child: Column(children: [
-                        Stack(children: [
-                          AspectRatio(
-                            aspectRatio:
-                                _firstVideoController!.value.aspectRatio,
-                            child: VideoPlayer(_firstVideoController!),
-                          ),
-                          Visibility(
-                            visible: isHiddenSecondvideo,
-                            child: Positioned(
-                                right: 10,
-                                bottom: 5,
-                                child: FittedBox(
-                                  fit: BoxFit.fill,
-                                  clipBehavior: Clip.hardEdge,
-                                  child: SizedBox(
-                                      width: 200,
-                                      height: 150,
-                                      child:
-                                          VideoPlayer(_secondVideoController!)),
-                                )),
-                          )
-                        ]),
-                        Container(
-                          color: Colors.black87,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              IconButton(
-                                  icon: Icon(
-                                    _firstVideoController!.value.isPlaying
-                                        ? Icons.pause
-                                        : Icons.play_arrow,
-                                    size: _playerButtonSize,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () {
-                                    setState(() {
-                                      _firstVideoController!.value.isPlaying
-                                          ? _firstVideoController!.pause()
-                                          : _firstVideoController!.play();
-                                    });
-                                  }),
-                              IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    isHiddenSecondvideo = !isHiddenSecondvideo;
-                                  });
-                                },
-                                icon: Icon(
-                                  Icons.wash,
-                                  size: _playerButtonSize,
-                                  color: Colors.white,
+        appBar: AppBar(
+          title: const Text(' \"2 in 1 video proof of concept\"'),
+        ),
+        body: FutureBuilder(
+            future: _initFirstVideoPlayer,
+            builder: (context, snapshot) => snapshot.connectionState ==
+                    ConnectionState.waiting
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Center(
+                    child: Column(
+                        //crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 1080,
+                            child: Column(children: [
+                              Stack(children: [
+                                AspectRatio(
+                                  aspectRatio:
+                                      _firstVideoController!.value.aspectRatio,
+                                  child: VideoPlayer(_firstVideoController!),
                                 ),
-                              )
-                            ],
+                                Visibility(
+                                  visible: isHiddenSecondvideo,
+                                  child: Positioned(
+                                      right: 10,
+                                      bottom: 5,
+                                      child: FittedBox(
+                                        fit: BoxFit.fill,
+                                        clipBehavior: Clip.none,
+                                        child: SizedBox(
+                                            width: 210,
+                                            height: 160,
+                                            child: VideoPlayer(
+                                                _secondVideoController!)),
+                                      )),
+                                ),
+                              ]),
+                              Container(
+                                alignment: Alignment.center,
+                                height: _playerButtonSize + 10,
+                                color: Colors.black87,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    IconButton(
+                                        icon: Icon(
+                                          _firstVideoController!.value.isPlaying
+                                              ? Icons.pause
+                                              : Icons.play_arrow,
+                                          size: _playerButtonSize,
+                                          color: Colors.white,
+                                        ),
+                                        onPressed: () {
+                                          setState(() {
+                                            _firstVideoController!
+                                                    .value.isPlaying
+                                                ? _firstVideoController!.pause()
+                                                : _firstVideoController!.play();
+                                          });
+                                        }),
+                                    IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          isHiddenSecondvideo =
+                                              !isHiddenSecondvideo;
+                                        });
+                                      },
+                                      icon: Icon(
+                                        SignlanguageIcon.signLanguage,
+                                        size: _playerButtonSize,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    IconButton(
+                                        icon: Icon(
+                                          Icons.loop,
+                                          size: _playerButtonSize,
+                                          color: _firstVideoController!
+                                                  .value.isLooping
+                                              ? Colors.yellow
+                                              : Colors.white,
+                                        ),
+                                        onPressed: () {
+                                          setState(() {
+                                            _firstVideoController!
+                                                    .value.isLooping
+                                                ? _firstVideoController!
+                                                    .setLooping(false)
+                                                : _firstVideoController!
+                                                    .setLooping(true);
+                                          });
+                                        })
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              const Text(
+                                  ' \"Proof of concept\" för att ha uppspelning av en video med teckentolking i en ruta i. Det är planerad att teckenrutan ska gå att flytta runt och förstora och förminska.\n Klicka på \"handen\" för att visa/dölja rutan ')
+                            ]),
                           ),
-                        ),
-                        const Text(
-                            ' \"Proof of concept\" för att ha uppspelning av en video med teckentolking i en ruta i. Det är planerad att teckenrutan ska gå att flytta runt och förstora och förminska.\n Klicka på \"handen\" för att visa/dölja rutan ')
-                      ]),
-                    ),
-                  ]),
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-    );
+                        ]),
+                  )));
   }
 }
---
+++ lib/widget/sign_language_icon.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class SignlanguageIcon {
+  SignlanguageIcon._();
+
+  static const _kFontFam = 'signlanguageicon';
+  static const String? _kFontPkg = null;
+
+  static const IconData signLanguage =
+      IconData(0xf2a7, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
